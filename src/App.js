f61fe1f4a169b97b089b2391e520d2596e36a27d
navya <navyasrirapeti1@gmail.com>
2022-07-01 23:53:29 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+export const App = () => {
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+  });
+  const { username, password } = data;
+  const changeHandler = e => {
+    setData({ ...data, [e.target.name]: [e.target.value] })
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form onSubmit={submitHandler} >
+          <input type="text" name="username" value={username}
+            onChange={changeHandler}
+            placeholder="Username" /><br />
+          <input type="text" name="password" value={password}
+            onChange={changeHandler}
+           placeholder="password" /><br />
+          <input type="submit" name="submit" />
+
+        </form>
+      </center>
     </div>
   );
 }
-
-export default App;
+export default App
