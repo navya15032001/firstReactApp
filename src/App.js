e42dd79a4f64318f9beb8bf7dc44e8a54a6c6767
navya <navyasrirapeti1@gmail.com>
2022-07-02 00:31:49 +0530

    Added filter code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export const App = () => {
-  const [data, setData] = useState({
-    username: '',
-    password: '',
-  });
-  const { username, password } = data;
-  const changeHandler = e => {
-    setData({ ...data, [e.target.name]: [e.target.value] })
-  }
-  const submitHandler = e => {
-    e.preventDefault();
-    console.log(data);
-  }
+  const names =['navya','ramya','swathi','radha'];
+  const filtered= names.filter(name => name.includes('r'))
   return (
     <div>
-      <center>
-        <form onSubmit={submitHandler} >
-          <input type="text" name="username" value={username}
-            onChange={changeHandler}
-            placeholder="Username" /><br />
-          <input type="text" name="password" value={password}
-            onChange={changeHandler}
-           placeholder="password" /><br />
-          <input type="submit" name="submit" />
-
-        </form>
-      </center>
-    </div>
-  );
+{
+  filtered.map(item => <li>{item}</li>)
 }
-export default App
+    </div>
+  )
+}
+export default App;
